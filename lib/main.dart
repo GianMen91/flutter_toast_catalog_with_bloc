d17246c7e49c6e4920c7a57fe9b69c53491d942f
GianMen91 <giancarlo.mennillo@gmail.com>
2024-08-20 10:57:00 +0200

    add comments to main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'src/bloc/item_bloc.dart';
-import 'src/screens/item_screen.dart';
+import 'src/bloc/item_bloc.dart';  // Importing the BLoC for managing state.
+import 'src/screens/item_screen.dart';  // Importing the screen where items will be displayed.
 
 void main() {
   runApp(
     BlocProvider(
-      create: (context) => ItemBloc()..add(LoadItemsEvent()),
-      child: const MyApp(),
+      // Providing the ItemBloc to the widget tree. This makes it available to all descendant widgets.
+      create: (context) => ItemBloc()..add(LoadItemsEvent()),  // Creating an instance of ItemBloc and dispatching LoadItemsEvent to load items.
+      child: const MyApp(),  // Passing the MyApp widget as the child of BlocProvider.
     ),
   );
 }
@@ -19,9 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Toast Catalog',
-      home: ItemScreen(),
-      debugShowCheckedModeBanner: false
+        title: 'Toast Catalog',  // Title of the application, displayed in the app switcher and other places.
+        home: ItemScreen(),  // Setting ItemScreen as the home widget, which is the first screen displayed.
+        debugShowCheckedModeBanner: false  // Hides the debug banner in the app.
     );
   }
 }
