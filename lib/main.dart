76125ec1cc474d6968942993109eb754742f8f01
GianMen91 <giancarlo.mennillo@gmail.com>
2024-08-20 09:55:59 +0200

    remove debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Toast Catalog',
-      home: const ItemScreen(),
+      home: ItemScreen(),
+      debugShowCheckedModeBanner: false
     );
   }
 }
